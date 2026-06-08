45e37e755e90c709ffc3e92e8f8ff45e92c79157
LuizFirias <lfernando.irias@outlook.com>
2026-06-08 18:30:22 -0300

    chore: add 172.20.10.2 to allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(__dirname),
   },
-  allowedDevOrigins: ['192.168.15.20', '192.168.15.21', '192.168.15.7', '192.168.15.19', '192.168.15.22', '192.168.15.12'],
+  allowedDevOrigins: ['192.168.15.20', '192.168.15.21', '192.168.15.7', '192.168.15.19', '192.168.15.22', '192.168.15.12', '172.20.10.2'],
   images: {
     remotePatterns: [
       {
